27823b122705a2323caecc26802b2e8eb4e83c0d
shiva <shivamgeeks@gmail.com>
2024-06-09 14:22:47 +0530

    Shivam comment

--- Abhijit.py
+++ Abhijit.py
@@ -5,3 +5,15 @@ def check_even(num):
 
 num = list(range(1,21))
 check_even(num)
+
+list1 = []
+
+def check_odd(num):
+    for n in num:
+        if n%2 != 0:
+            list1.append(n)
+    return list1
+
+num = list(range(1,31))
+
+print(check_odd(num))
